c5e71ea583e3d7389794c5edc4a87c454669c668
Zetto <tomas.skarecky@gmail.com>
2012-02-19 12:48:10 +0100

    * forgotten files

---
+++ ZetSwitch/Src/Forms/InterfaceListBox.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ZetSwitch {
+	public class InterfaceListBox : CheckedListBox {
+
+		Timer timer = new Timer();
+		bool loaded = false;
+		int dotsCount = 1;
+
+		public InterfaceListBox() {
+			timer.Interval = 500;
+			timer.Tick += new EventHandler(timer_Tick);
+			this.SetStyle(ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint , true);
+			timer.Start();
+		}
+
+		public bool IsLoaded {
+			set { 
+				loaded = value;
+				if (loaded)
+					timer.Stop();
+				this.SetStyle(ControlStyles.UserPaint, false);
+				timer.Dispose();
+			}
+		}
+
+		private void timer_Tick(object o, EventArgs e) {
+			dotsCount++;
+			if (dotsCount > 3)
+				dotsCount = 1;
+			Refresh();
+		}
+
+		protected override void OnPaint(PaintEventArgs e) {
+			Font titleFont = new Font("Ariel", 10);
+			Brush solid = new SolidBrush(Color.Black);
+			string label = Language.GetText("loading");
+			Size textSize = TextRenderer.MeasureText(label, titleFont);
+			for (int i = 0; i < dotsCount; i++) {
+				label += ".";
+			}
+			try {
+				Point p = new Point((Width - textSize.Width) / 2, (Height - textSize.Height) / 2);
+				e.Graphics.DrawString(label, titleFont, solid, p);
+			} finally {
+				titleFont.Dispose();
+				solid.Dispose();
+			}
+		}
+
+		protected override void Dispose(bool disposing) {
+			base.Dispose(disposing);
+			if (disposing)
+				timer.Dispose();
+		}
+	}
+}
---
+++ ZetSwitch/Src/ShorcutCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IWshRuntimeLibrary;
+using System.IO;
+
+namespace ZetSwitch {
+	class ShorcutCreator {
+		public void CreateProfileLnk(Profile profile) {
+			WshShellClass wshShell = new WshShellClass();
+			IWshRuntimeLibrary.IWshShortcut shortcut;
+			shortcut = (IWshRuntimeLibrary.IWshShortcut)wshShell.CreateShortcut(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)+"\\"+profile.Name+".lnk");
+			shortcut.TargetPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+			shortcut.WorkingDirectory = new FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).Directory.FullName;
+			shortcut.Arguments = "-p " + profile.Name;
+			shortcut.Save();
+		}
+	}
+}
